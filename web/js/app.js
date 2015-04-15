fc9f3c9f51291039c76bc49719ae9eedd76d07db
Hay Kranen <huskyr@gmail.com>
2015-04-15 16:34:16 +0200

    Implemented swipebox instead of photoswipe, adding some license stuff as well

--- web/js/app.js
+++ web/js/app.js
@@ -1,63 +1,4 @@
 (function() {
-    function loadWorkImage() {
-        var $itemImage = $(".item-image");
-        if (!$itemImage.length) return;
-
-        var src = $itemImage.data('src');
-
-        var img = new Image();
-
-        img.onload = function() {
-            $itemImage.attr('style', "background-image:url('" + src + "')");
-            $itemImage.addClass('item-image-loaded');
-        };
-
-        img.src = src;
-    }
-
-    function photoswipe() {
-        var $pswp = $(".pswp");
-        var items = [];
-        var $images = $(".img-zoomable");
-
-        $images.each(function() {
-            var $el = $(this);
-
-            items.push({
-                src : $el.attr('src').replace('width=500', 'width=1024'),
-                msrc : $el.attr('src'),
-                w : 1024,
-                h : 1024
-            });
-        });
-
-        $(".img-zoomable").on('click', function() {
-            var index = $images.index(this);
-            var $el = $(this);
-
-            var gallery = new PhotoSwipe(
-                $pswp.get(0),
-                PhotoSwipeUI_Default,
-                items,
-                {
-                    index : index,
-                    getThumbBoundsFn: function(index) {
-                        var pos = $el.offset();
-
-                        return {
-                            x : pos.left,
-                            y : pos.top,
-                            w : $el.width()
-                        };
-                    }
-                }
-            );
-
-            gallery.init();
-            gallery.zoomTo(1, {x:gallery.viewportSize.x/2,y:gallery.viewportSize.y/2}, 200);
-        });
-    }
-
     function searchclear() {
         $('[data-action="clearinput"]').each(function() {
             var $el = $(this);
@@ -82,8 +23,11 @@
         FastClick.attach(document.body);
     }
 
+    function swipebox() {
+        $(".img-zoomable").swipebox();
+    }
+
     fastClick();
-    loadWorkImage();
-    photoswipe();
     searchclear();
+    swipebox();
 })();
