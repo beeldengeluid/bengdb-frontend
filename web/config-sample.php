447753bf1d699806bb0e5784c7aa154e9968b349
Hay Kranen <huskyr@gmail.com>
2015-04-15 15:40:31 +0200

    Adding credits for WP articles, removing all other languages than Dutch / English

--- web/config-sample.php
+++ web/config-sample.php
@@ -1,35 +1,13 @@
 <?php
     define('PATH', str_replace("/index.php", "", $_SERVER['PHP_SELF']));
-    define('ROOT', sprintf(
-        "//%s/%s/",
-        $_SERVER['HTTP_HOST'],
-        str_replace("/index.php", "", $_SERVER['PHP_SELF'])
-    ));
+    define('ROOT', "//localhost/git/beng/bengdb-frontend/web"); // NO trailing slash!
     define('API_ENDPOINT', 'http://localhost:5000');
     define('DEFAULT_LANGUAGE', 'nl');
     define('DEBUG', false);
 
     class Config {
         public static $primaryLanguages = [
-          "en",
-          "ar",
-          "ca",
-          "ceb",
-          "da",
-          "de",
-          "es",
-          "fr",
-          "hu",
-          "it",
-          "ja",
           "nl",
-          "no",
-          "pl",
-          "pt",
-          "ru",
-          "sv",
-          "vi",
-          "war",
-          "zh"
+          "en"
         ];
     }
